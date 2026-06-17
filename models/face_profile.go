1992b71c91aed035aa20396db54fc8178e27bc5c
Lallene Cedric <lallene2016@gmail.com>
2026-06-17 23:15:40 +0200

    Allow nullable embedding path during migration

--- models/face_profile.go
+++ models/face_profile.go
@@ -6,7 +6,7 @@ type FaceProfile struct {
 	ID             uint       `gorm:"primaryKey" json:"id"`
 	UserID         uint       `gorm:"not null" json:"user_id"`
 	User           User       `gorm:"foreignKey:UserID" json:"user"`
-	EmbeddingPath  string     `gorm:"not null" json:"embedding_path"`
+	EmbeddingPath  string     `json:"embedding_path"`
 	ImagePath      string     `json:"image_path"`
 	ImageExpiresAt *time.Time `json:"image_expires_at"`
 	ImageDeletedAt *time.Time `json:"image_deleted_at"`
