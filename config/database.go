d64b928c767751896a4d9676e18512fb75e177b2
Lallene Cedric <lallene2016@gmail.com>
2026-06-17 19:42:13 +0200

    Add configurable database SSL mode

--- config/database.go
+++ config/database.go
@@ -14,14 +14,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
+	sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
-		
+		sslmode,
 	)
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
